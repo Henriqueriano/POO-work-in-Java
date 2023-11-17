f031cccbb1ac55c18d2353a4945728eb994dd760
Daniel Henrique <113483288+Henriqueriano@users.noreply.github.com>
2023-11-17 00:29:16 -0300

    Add files via upload

--- Funcionario.java
+++ Funcionario.java
@@ -5,7 +5,7 @@ public class Funcionario extends Pessoa
     private String dataAdmissao;
     private String cargo;
     
-    public Funcionario(int id, String departamento, String dataAdmissao, String cargo)
+    public Funcionario(int id, String departamento, String dataAdmissao, String cargo, String documento, String nome, String sexo, String dataNascimento, String cep, String endereco, String telefone)
     {
         super(sexo, cep, documento, nome, dataNascimento, endereco, telefone);
         this.id 			 = 0;
