52ee4380edfc297066eb8e798b9a85d54ae7d187
Daniel Henrique <daniel.neves0021@gmail.com>
2023-11-19 16:15:14 -0300

    Esse daqui da pra entregar por via das duvidas

---
+++ Main.java
@@ -0,0 +1,119 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+abstract class Pessoa 
+{
+	protected String nome;
+	public Pessoa(String nome) 
+	{
+		this.nome = nome;
+	}
+	
+}
+
+class Funcionario extends Pessoa
+{
+	protected int numFuncional;
+	public Funcionario(String nome, int numFuncional) 
+	{
+		super(nome);
+		this.numFuncional = numFuncional;
+	}
+}
+
+class Produto 
+{
+	private String nome;
+	private String modelo;
+	private double valor;
+	public Produto(String nome, String modelo, double valor) {
+		this.nome = nome;
+		this.modelo = modelo;
+		this.valor = valor;
+	}
+	
+	public double getValor() {
+		return this.valor;
+	}
+	
+	public String getNome() { return this.nome;}
+	public String getModelo() { return this.modelo;}
+}
+
+class Compra 
+{
+	private ArrayList<Produto> produto = new ArrayList<>();
+	private int numNF;
+	private int i = 0;
+	private double total;
+	public Compra() {
+		this.numNF = (int) (Math.random() * 100);
+	}
+	
+	// public int getNF() { return this.numNF;}
+	
+	public void addProdutos(String nome, String modelo, double valor) {
+		produto.add( new Produto(nome, modelo, valor));
+	}
+	
+	public void listarProdutos() {
+		System.out.println("\nListando todos os produtos:");
+		for (Produto produto : produto) 
+		{
+			i++;
+			System.out.printf("+=---=+=---=+=---=+=---=+=---=+=---=+\nNome: %s\nModelo: %s\nValor: R$%.2f\n+=---=+=---=+=---=+=---=+=---=+=---=+",produto.getNome(),produto.getModelo(),produto.getValor());
+		}
+		System.out.printf("\nTotal de produtos na compra: %d", i);
+		System.out.printf("\nPróxima escolha: ");
+		 
+	}
+	
+	public void finalizarVenda() {
+		for (Produto produto : produto) 
+		{
+			total += produto.getValor();
+		}
+		System.out.printf("\nTotal de produtos na compra: %d", i);
+		System.out.printf("\nTotal da compra: R$%.2f", total);
+		System.out.printf("\nNota fiscal gerada: %d", (int) Math.round(numNF) );
+	}
+}
+
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		Compra c = new Compra();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("ERP de Loja by Brenin and Daniel H.");
+		System.out.println("+=-------------------------------=+");
+		System.out.println("Para realizar uma compra digite 0;\nPara listar os produtos de sua compra atual digite1;\nPara Finalizar digite 2;");
+		System.out.print("Sua escolha: ");
+		try {
+			boolean fun = true;
+			while(fun){
+				int choice = sc.nextInt();
+				switch (choice) {
+					case 0:
+						double valor = Math.random() * 10;
+						System.out.println("\nInforme o nome do produto: ");
+						String name = sc.next();
+						System.out.println("Informe o modelo do produto: ");
+						String model = sc.next();
+						c.addProdutos(name, model, valor);
+						System.out.print("Produto Cadastrado! \nPróxima escolha: ");
+					break;
+					case 1:
+					c.listarProdutos();
+					break;
+					case 2:
+					c.finalizarVenda();
+					fun = false;
+					break;
+				}
+			}
+		} catch (Exception e){ System.out.println("Erro de entrada!");} finally {
+			sc.close();
+		}
+	}
+}
